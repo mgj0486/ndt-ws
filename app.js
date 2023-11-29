1e6f9359419b74956e0a59b1fb59873552e0f4e9
MOOQ <jungmk9525@gmail.com>
2023-11-29 14:48:36 +0900

    etc

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const webserver = express()
  .listen(3000, () => console.log(`Listening on ${3000}`))
 const { WebSocketServer } = require('ws')
 const sockserver = new WebSocketServer({ port: 8000 })
+
 sockserver.on('connection', ws => {
- console.log('New client connected!')
- ws.send('connection established')
- ws.on('close', () => console.log('Client has disconnected!'))
- ws.on('message', data => {
-   sockserver.clients.forEach(client => {
-     console.log(`distributing message: ${data}`)
-     client.send(`받은 메시지 : ${data}`)
-   })
- })
- ws.onerror = function () {
-   console.log('websocket error')
- }
+  ws.send('connection successed')
+
+  ws.on('close', () => 
+    console.log('Client has disconnected!')
+  )
+
+  ws.on('message', data => {
+    sockserver.clients.forEach(client => {
+       console.log(`distributing message: ${data}`)
+       client.send('${data}')
+    })
+  })
+
+  ws.onerror = function () {
+     console.log('websocket error')
+  }
 })
